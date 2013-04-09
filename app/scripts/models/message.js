4cc2c98f3e2f763d3f1efe7f93b02be56440007d
Tim Park <timfpark@gmail.com>
2013-04-08 20:25:59 -0700

    add access token to end of url for url based auth for images

--- app/scripts/models/message.js
+++ app/scripts/models/message.js
@@ -9,7 +9,14 @@ App.Message = Ember.Object.extend({
 
     isLog: function() {
         return this.is('log');
-    }.property('message_type')
+    }.property('message_type'),
+
+    // TODO: build message specific views that encapsulate this.
+    bodyUrlWithAccessToken: function() {
+        if (!this.get('body') || !this.get('body.url')) return null;
+
+        return this.get('body.url') + "?access_token=" + encodeURIComponent(App.session.accessToken.token);
+    }.property('body.url')
 });
 
 App.Message.reopenClass({
