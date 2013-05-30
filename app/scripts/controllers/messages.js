ff41c42493b1121ccc1e9cf1cccde48e02ef7671
Tim Park <timfpark@gmail.com>
2013-05-30 07:11:48 -0700

    implement device claim

--- app/scripts/controllers/messages.js
+++ app/scripts/controllers/messages.js
@@ -1,5 +1,24 @@
 App.MessagesController = Ember.ArrayController.extend({
  	onMessage: function(message) {
         this.unshiftObject(message);
+    },
+
+    claim: function() {
+        var principalId = $('principalId').val();
+
+        console.log("claiming device: " + principalId);
+
+        var claim = new nitrogen.Message({
+            to: 'system',
+            type: 'claim',
+            body: {
+                principal: principalId
+            },
+            expires: 'never'
+        });
+
+        response.save(App.session, function(err, messages) {
+            if (err) console.log(err);
+        });
     }
 });
