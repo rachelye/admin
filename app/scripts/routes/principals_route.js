ea70d74f0cd670b9547aac5bf4ae1bb4eac6e65c
Tim Park <timfpark@gmail.com>
2013-05-30 07:30:32 -0700

    principal_type -> type

--- app/scripts/routes/principals_route.js
+++ app/scripts/routes/principals_route.js
@@ -1,7 +1,7 @@
 App.PrincipalsRoute = Ember.Route.extend({
 
     model: function(params) {
-        return App.Principal.find({ principal_type: params.principal_type });
+        return App.Principal.find({ type: params.type });
     }
 
 });
--- app/scripts/routes/router.js
+++ app/scripts/routes/router.js
@@ -1,7 +1,7 @@
 App.Router.map(function() {
     this.resource('agents');
     this.resource('messages');
-    this.resource('principals', { path: '/principals/:principal_type' });
+    this.resource('principals', { path: '/principals/:type' });
     this.resource('user', function() {
         this.route('create');
         this.route('login');
