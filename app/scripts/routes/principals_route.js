75cccc7b6ed9517019827f31b0dfe66016529692
Tim Park <timfpark@gmail.com>
2013-06-18 07:32:13 -0700

    automatically update principals on max interval

--- app/scripts/routes/principals_route.js
+++ app/scripts/routes/principals_route.js
@@ -1,6 +1,60 @@
 App.PrincipalsRoute = Ember.Route.extend({
+    pageLimit: 50,
+    maxUpdateRate: 10000,
+    timeoutSet: false,
+    nextUpdate: new Date(),
+
     model: function(params) {
-        return App.Principal.find({});
+        // TODO: implement sorting and paging
+        params = {
+            sort: 'last_connection',
+            direction: -1,
+            skip: 0
+        };
+
+        this.set('params', params);
+        return this.query();
+    },
+
+    query: function() {
+        var sort = {};
+        sort[this.get('params').sort] = parseInt(this.get('params').direction);
+
+        return App.Principal.find({}, {
+            skip: parseInt(this.get('params').skip),
+            limit: parseInt(this.get('pageLimit')),
+            sort: sort
+        });
+    },
+
+    setupController: function(controller, model) {
+        this._super(controller, model);
+
+        this.controller.set('router', this);
+
+        var self = this;
+        App.session.onPrincipal(function(nitrogenMessage) {
+            // if we already are locked and loaded to update, just return.
+            if (self.get('timeoutSet')) {
+                console.log('principals timeout already set, returning.')
+                return;
+            }
+
+            // only update at most once every N seconds.
+            var updateTimeout = Math.max(0, self.get('nextUpdate').getTime() - new Date().getTime());
+            console.log('updating principals in ' + updateTimeout);
+
+            setTimeout(function() {
+                self.query().then(function(principals) {
+                    self.controller.set('content', principals);
+                });
+
+                self.set('nextUpdate', new Date(new Date().getTime() + self.get('maxUpdateRate')));
+                self.set('timeoutSet', false);
+            }, updateTimeout);
+
+            self.set('timeoutSet', true);
+        });
     }
 });
 
