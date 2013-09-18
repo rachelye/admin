8bd8ca26392abbfda8bb5dab8b4d44f4f9464caa
Tim Park <timfpark@gmail.com>
2013-09-17 20:40:46 -0700

    add rough device claiming ui to admin

---
+++ app/scripts/views/principals/claim.js
@@ -0,0 +1,24 @@
+App.ClaimView = Em.View.extend({
+    templateName: 'principals/claim',
+
+    actions: {
+        claim: function() {
+            var response = new nitrogen.Message({
+                to: 'service',
+                type: 'claim',
+                body: {
+                    claim_code: this.get('claimCode').toUpperCase()
+                },
+                expires: 'never'
+            });
+
+            console.dir(response);
+            response.send(App.session, function(err) {
+                if (err) App.set('flash', err.message);
+
+                window.location.reload();
+            });
+        }
+    }
+
+});
