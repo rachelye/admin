2eddfe9a89c031dddcc73df3420dc35d53aabd3d
Tim Park <timfpark@gmail.com>
2013-09-19 20:23:18 -0700

    fix flash message and session clearing

--- app/scripts/controllers/application.js
+++ app/scripts/controllers/application.js
@@ -2,8 +2,8 @@ App.ApplicationController = Ember.Controller.extend({
     actions: {
         signout: function() {
             App.session.clearCredentials();
-            App.set('session', null);
             this.transitionToRoute('login');
+            App.set('session', null);
         }        
     }
 });
