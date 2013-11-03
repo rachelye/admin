fef51201391d708126f0a4a4ea27109fae567f40
Tim Park <timfpark@gmail.com>
2013-11-03 15:32:43 -0800

    check in switch capability UI

---
+++ app/scripts/views/capabilities/switch.js
@@ -0,0 +1,50 @@
+App.SwitchCapabilityView = Em.View.extend({
+    templateName: 'capabilities/switch',
+
+    actions: {
+        sendSwitch: function() {
+            var self = this;
+            var newState = this.switchManager.state === 0.0 ? 1.0 : 0.0;
+
+            var command = new nitrogen.Message({
+                to: this.get('principal.id'),
+                type: 'switchCommand',
+                body: {
+                    command: {
+                        on: newState
+                    }
+                }
+            });
+
+            command.send(App.session, function(err, messages) {
+                if (err) console.log('sending command failed: ' + err);
+            });
+        },
+        sendMotion: function() { this.sendCommand('motion'); }
+    },
+
+    commands: function() {
+
+        var ret = Em.A();
+        if (!this.switchManager) return Em.A([]);
+
+        this.switchManager.messageQueue.forEach(function(message) {
+            ret.pushObject(message);
+        });
+
+        return ret;
+    }.property('invalidation'),
+
+    invalidation: null,
+
+    init: function() {
+        this.switchManager = new nitrogen.SwitchManager();
+        var self = this;
+
+        var principalId = this.get('principal.id');
+
+        this.switchManager.start(App.session, { $or: [ { to: principalId }, { from: principalId } ] }, function(err, message) {
+            self.set('invalidation', new Date());
+        });
+    }
+});
