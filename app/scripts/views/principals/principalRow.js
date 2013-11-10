55806535fa56b28369985a3053b39710c93ef545
Tim Park <timfpark@gmail.com>
2013-11-10 13:15:04 +0800

    remove unnecessary PrincipalRow

--- app/scripts/views/principals/principalRow.js
+++
@@ -1,8 +0,0 @@
-App.PrincipalRowView = Em.View.extend({
-    templateName: 'principals/principalRow',
-
-    lastConnectionClass: function() {
-        var msSinceLastConnection = new Date().getTime() - Date.parse(this.get('principal.last_connection'));
-        return msSinceLastConnection > 30 * 60 * 1000 ? "text-error" : "";
-    }.property('principal.last_connection')
- });
