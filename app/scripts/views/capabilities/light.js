715c35b41331dfee6272d96e801ed47a9c032e48
Rachel Yehezkel <rachelyehe@hotmail.com>
2014-09-02 10:11:13 +0200

    adding lightCapable

--- app/scripts/views/capabilities/light.js
+++ app/scripts/views/capabilities/light.js
@@ -47,7 +47,8 @@ App.LightCapabilityView = Em.View.extend({
                 type: 'lightCommand',
                 tags: [nitrogen.CommandManager.commandTag(lightId)],
                 body: {
-                    brightness: 0.5
+                    brightness: 0.5,
+                    frequency: 50
                 }
             });
 
